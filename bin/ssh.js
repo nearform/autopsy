816c6aa27be5d9baf55663a08eb364201ebe0692
davidmarkclements <huperekchuno@googlemail.com>
2015-11-28 19:07:03 +0000

    bug fix

--- bin/ssh.js
+++ bin/ssh.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn
-var port = 8080
+var port = 2222
 var tunnel = ['-R', port + ':127.0.0.1:' + port]
+var debug = require('debug')('autopsy:ssh')
 
 module.exports = function sshCmd(args) {
-  spawn('ssh', tunnel.concat(args), {stdio: 'inherit'})  
+  debug('spawn: ssh ' + tunnel.concat(args).join(' '))
+  spawn('ssh', tunnel.concat(args), {stdio: 'inherit'})
 }
 
 if (!module.parent) { module.exports(process.argv.slice(2)) }
