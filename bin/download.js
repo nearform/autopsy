3a8bec4aa0665e75fd82f404777856daee020add
davidmarkclements <huperekchuno@googlemail.com>
2015-11-28 02:48:01 +0000

    no dl in virt

--- bin/download.js
+++ bin/download.js
@@ -1,3 +1,13 @@
 var download = require('../lib/download')
 var assets = require('../lib/assets-uri')
+
+var virtualized = require('in-virtualized-os')()
+
+if (virtualized) {
+  console.error('This is a virtualized environment, can\'t set up a vm in a vm')
+  console.error('However we can setup autopsy on our local machine and tunnel through')
+  console.error('Checkout the autopsy-ssh command for more info')
+  return
+}
+
 download(assets)
